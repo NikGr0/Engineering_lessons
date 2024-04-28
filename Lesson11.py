7e0c11118431dfda68cf28f13651f24c70398f9b
Никита <nikita0112358@yandex.ru>
2024-04-28 10:28:25 +0500

    Добавил все уроки

---
+++ Lesson11.py
@@ -0,0 +1,11 @@
+
+
+class User:
+    def __init__(self):
+        d_user = [{'name': 'Ivan',
+                  'age': '28',
+                  'e_mail': 'ivan@mail.com'},
+                 {'name': 'Petr',
+                  'age': '33',
+                  'e_mail': 'petr@mail.com'}]
+
